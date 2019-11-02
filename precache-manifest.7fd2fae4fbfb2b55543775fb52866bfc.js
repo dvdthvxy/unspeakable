ac6f3bcc8f050cfaa349fcc23943a222dbc43f2b
David Thavixay <david.thavixay@gmail.com>
2019-11-01 23:31:21 -0400

    Updates

---
+++ precache-manifest.7fd2fae4fbfb2b55543775fb52866bfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e97c187ee753e782861c8bb70d97cee",
+    "url": "/dvdthvxy/unspeakable/index.html"
+  },
+  {
+    "revision": "8bb85b6a2ad7e16da364",
+    "url": "/dvdthvxy/unspeakable/static/css/main.30bd087e.chunk.css"
+  },
+  {
+    "revision": "2b4d37a7786ed8d4a8b2",
+    "url": "/dvdthvxy/unspeakable/static/js/2.8f08b5bf.chunk.js"
+  },
+  {
+    "revision": "8bb85b6a2ad7e16da364",
+    "url": "/dvdthvxy/unspeakable/static/js/main.0a1ec9d6.chunk.js"
+  },
+  {
+    "revision": "b89a75db2d17d8977307",
+    "url": "/dvdthvxy/unspeakable/static/js/runtime-main.77728096.js"
+  }
+]);
